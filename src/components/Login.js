d1d1d2159b153de0e48e4ff9ff20abb637a32882
Tejaswini <velpulatejaswini2002@gmail.com>
2023-03-03 17:45:04 +0530

    second commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,10 @@ function Login() {
     <div className='m-5 ps-5'>
     <h3 className="font-weight-bold p-4 text-dark text-primary mb-5">Login</h3>
 
-    <Form.Group as={Row} className="mb-3 w-50 mx-auto" controlId="formBasicEmail">
-        <Form.Label column sm={1} className="d-flex justify-content-end">Email</Form.Label>
+    <Form.Group as={Row} className="mb-3 w-50 mx-auto" controlId="formUsername">
+        <Form.Label column sm={1} className="d-flex justify-content-end">Username</Form.Label>
         <Col sm={11}>
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Control type="text" placeholder="Username" />
         </Col>
       </Form.Group>
       <Form.Group as={Row} className="mb-3 w-50 mx-auto" controlId="formBasicPassword">
